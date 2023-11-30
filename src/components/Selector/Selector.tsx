8cf96b4144ff364441052d17005b7b9b4a4a3c27
Tomasz Kalinowski <tomasz.kalinowski91@gmail.com>
2023-11-30 21:41:54 +0100

    move selector logic to hook

--- src/components/Selector/Selector.tsx
+++ src/components/Selector/Selector.tsx
@@ -1,12 +1,10 @@
-import React, { useCallback, useEffect, useRef, useState } from "react"
+import React from "react"
 
 import { PreviewStage } from "./PreviewStage"
-import { KonvaEventObject } from "konva/lib/Node"
 import { MainStage } from "./MainStage"
-import { Vector2d } from "konva/lib/types"
-import { Point, Shelf, NewPoint, FourPoints } from "./types"
-import { debounce } from "../../helpers.ts/debounce"
-import { getTsId } from "../../helpers.ts/utils"
+import { Shelf } from "./types"
+
+import { useSelector } from "./useSelector"
 
 type SelectorProps = {
   imageUrl: string
@@ -15,131 +13,24 @@ type SelectorProps = {
 }
 
 export const Selector: React.FC<SelectorProps> = ({ imageUrl, shelves: initialShelves, onChange }) => {
-  const [pointer, setPointer] = useState<Point | null>(null)
-  const [shelves, setShelves] = useState<Shelf[]>(() => initialShelves || [])
-  const [activeShelf, setActiveShelf] = useState<Shelf | null>(null)
-  const [newShelf, setNewShelf] = useState<NewPoint | null>(null)
-  const [activePointId, setActivePointId] = useState<string | null>(null)
-  const timer = useRef<number | null>(null)
-  const activePoint = useRef<boolean>(false)
-  const debouncedOnChange = useCallback(debounce(onChange, 200), [onChange])
-
-  const handleMouseDown = (event: KonvaEventObject<MouseEvent>) => {
-    if (activePoint.current) {
-      setNewShelf(null)
-      return
-    }
-    if (!newShelf) {
-      const stage = event.target.getStage()
-      if (!stage) return
-
-      const { x, y } = stage.getPointerPosition() as Vector2d
-      setNewShelf({ x, y, xd: 0, yd: 0 })
-      timer.current = new Date().getTime()
-    }
-  }
-
-  const handleMouseUp = (event: KonvaEventObject<MouseEvent>) => {
-    if (timer.current === null) return
-    activePoint.current = false
-    const now = new Date().getTime()
-    const diff = now - timer.current
-    if (diff < 200) {
-      timer.current = null
-      setNewShelf(null)
-      return
-    }
-    if (activePointId) {
-      setActivePointId(null)
-      return
-    }
-    if (newShelf) {
-      const sx = newShelf.x
-      const sy = newShelf.y
-      const stage = event.target.getStage()
-      if (!stage) return
-      const { x, y } = stage.getPointerPosition() as Vector2d
-      const annotationToAdd: Shelf = {
-        position: [
-          { x, y, id: "1" },
-          { x, y: sy, id: "2" },
-          { x: sx, y: sy, id: "3" },
-          { x: sx, y, id: "4" },
-        ],
-        id: getTsId(),
-      }
-      shelves.push(annotationToAdd)
-      setNewShelf(null)
-      setShelves(shelves)
-    }
-  }
-
-  const handleMouseMove = (event: KonvaEventObject<MouseEvent>) => {
-    activePoint.current = false
-    const stage = event.target.getStage()
-    if (!stage) return
-
-    if (activePointId) {
-      const { x, y } = stage.getPointerPosition() as Vector2d
-      setPointer({ x, y, id: "" })
-      if (activeShelf) {
-        setActiveShelf({
-          ...activeShelf,
-          position: activeShelf.position.map((point) =>
-            activePointId === point.id
-              ? {
-                  x,
-                  y,
-                  id: point.id,
-                }
-              : point
-          ) as FourPoints,
-        })
-        return
-      }
-    }
-    if (newShelf) {
-      const sx = newShelf.x
-      const sy = newShelf.y
-      const { x, y } = stage.getPointerPosition() as Vector2d
-      setNewShelf({
-        x: sx,
-        y: sy,
-        xd: x - sx,
-        yd: y - sy,
-      })
-    }
-  }
-
-  const onActivate = (shelfId: string) => {
-    const newActiveShelf = shelves.find((s) => s.id === shelfId)
-    if (!newActiveShelf) return
-    if (activeShelf && activeShelf.id !== newActiveShelf.id) {
-      setShelves((shelves) => [...shelves, activeShelf])
-    }
-    setShelves((shelves) => shelves.filter((s) => s.id !== shelfId))
-    setActiveShelf(newActiveShelf)
-  }
-
-  useEffect(() => {
-    const keyDownHandler = (event: KeyboardEvent) => {
-      if (event.key === "Backspace") {
-        event.preventDefault()
-        if (activeShelf) setActiveShelf(null)
-      }
-    }
-
-    document.addEventListener("keydown", keyDownHandler)
-
-    return () => {
-      document.removeEventListener("keydown", keyDownHandler)
-    }
-  }, [activeShelf])
-
-  useEffect(() => {
-    debouncedOnChange(activeShelf ? [...shelves, activeShelf] : shelves)
-  }, [shelves.length, activeShelf])
-
+  const {
+    pointer,
+    onActivate,
+    handleMouseDown,
+    handleMouseMove,
+    handleMouseUp,
+    newShelf,
+    shelves,
+    activeShelf,
+    activePointId,
+    setActivePointId,
+    setShelves,
+    setActiveShelf,
+    activePoint,
+  } = useSelector({
+    initialShelves,
+    onChange,
+  })
   return (
     <>
       <MainStage
---
+++ src/components/Selector/useSelector.ts
@@ -0,0 +1,154 @@
+import { useCallback, useEffect, useRef, useState } from "react"
+
+import { KonvaEventObject } from "konva/lib/Node"
+import { Vector2d } from "konva/lib/types"
+import { Point, Shelf, NewPoint, FourPoints } from "./types"
+import { debounce } from "../../helpers.ts/debounce"
+import { getTsId } from "../../helpers.ts/utils"
+
+type UseSelectorArgs = {
+  initialShelves?: Shelf[]
+  onChange: (shelves: Shelf[]) => void
+}
+export const useSelector = ({ initialShelves, onChange }: UseSelectorArgs) => {
+  const [pointer, setPointer] = useState<Point | null>(null)
+  const [shelves, setShelves] = useState<Shelf[]>(() => initialShelves || [])
+  const [activeShelf, setActiveShelf] = useState<Shelf | null>(null)
+  const [newShelf, setNewShelf] = useState<NewPoint | null>(null)
+  const [activePointId, setActivePointId] = useState<string | null>(null)
+  const timer = useRef<number | null>(null)
+  const activePoint = useRef<boolean>(false)
+  const debouncedOnChange = useCallback(debounce(onChange, 200), [onChange])
+
+  const handleMouseDown = (event: KonvaEventObject<MouseEvent>) => {
+    if (activePoint.current) {
+      setNewShelf(null)
+      return
+    }
+    if (!newShelf) {
+      const stage = event.target.getStage()
+      if (!stage) return
+
+      const { x, y } = stage.getPointerPosition() as Vector2d
+      setNewShelf({ x, y, xd: 0, yd: 0 })
+      timer.current = new Date().getTime()
+    }
+  }
+
+  const handleMouseUp = (event: KonvaEventObject<MouseEvent>) => {
+    if (timer.current === null) return
+    activePoint.current = false
+    const now = new Date().getTime()
+    const diff = now - timer.current
+    if (diff < 200) {
+      timer.current = null
+      setNewShelf(null)
+      return
+    }
+    if (activePointId) {
+      setActivePointId(null)
+      return
+    }
+    if (newShelf) {
+      const sx = newShelf.x
+      const sy = newShelf.y
+      const stage = event.target.getStage()
+      if (!stage) return
+      const { x, y } = stage.getPointerPosition() as Vector2d
+      const annotationToAdd: Shelf = {
+        position: [
+          { x, y, id: "1" },
+          { x, y: sy, id: "2" },
+          { x: sx, y: sy, id: "3" },
+          { x: sx, y, id: "4" },
+        ],
+        id: getTsId(),
+      }
+      shelves.push(annotationToAdd)
+      setNewShelf(null)
+      setShelves(shelves)
+    }
+  }
+
+  const handleMouseMove = (event: KonvaEventObject<MouseEvent>) => {
+    activePoint.current = false
+    const stage = event.target.getStage()
+    if (!stage) return
+
+    if (activePointId) {
+      const { x, y } = stage.getPointerPosition() as Vector2d
+      setPointer({ x, y, id: "" })
+      if (activeShelf) {
+        setActiveShelf({
+          ...activeShelf,
+          position: activeShelf.position.map((point) =>
+            activePointId === point.id
+              ? {
+                  x,
+                  y,
+                  id: point.id,
+                }
+              : point
+          ) as FourPoints,
+        })
+        return
+      }
+    }
+    if (newShelf) {
+      const sx = newShelf.x
+      const sy = newShelf.y
+      const { x, y } = stage.getPointerPosition() as Vector2d
+      setNewShelf({
+        x: sx,
+        y: sy,
+        xd: x - sx,
+        yd: y - sy,
+      })
+    }
+  }
+
+  const onActivate = (shelfId: string) => {
+    const newActiveShelf = shelves.find((s) => s.id === shelfId)
+    if (!newActiveShelf) return
+    if (activeShelf && activeShelf.id !== newActiveShelf.id) {
+      setShelves((shelves) => [...shelves, activeShelf])
+    }
+    setShelves((shelves) => shelves.filter((s) => s.id !== shelfId))
+    setActiveShelf(newActiveShelf)
+  }
+
+  useEffect(() => {
+    const keyDownHandler = (event: KeyboardEvent) => {
+      if (event.key === "Backspace") {
+        event.preventDefault()
+        if (activeShelf) setActiveShelf(null)
+      }
+    }
+
+    document.addEventListener("keydown", keyDownHandler)
+
+    return () => {
+      document.removeEventListener("keydown", keyDownHandler)
+    }
+  }, [activeShelf])
+
+  useEffect(() => {
+    debouncedOnChange(activeShelf ? [...shelves, activeShelf] : shelves)
+  }, [shelves.length, activeShelf])
+
+  return {
+    pointer,
+    onActivate,
+    handleMouseDown,
+    handleMouseMove,
+    handleMouseUp,
+    newShelf,
+    shelves,
+    setShelves,
+    activeShelf,
+    activePointId,
+    setActivePointId,
+    setActiveShelf,
+    activePoint,
+  }
+}
